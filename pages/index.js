5dca75b65f2ce7775444afe7a17278d725af53c7
bariabbassi <abbassibari@gmail.com>
2020-12-29 13:29:14 +0100

    login with github

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 import styled from 'styled-components';
-import Subscribe from '../components/ Subscribe';
-import { auth } from 'firebase-admin';
+
+import Subscribe from '@/components/ Subscribe';
+import { useAuth } from '@/lib/auth';
 
 const HeroSection = styled.section`
   width: 100%;
@@ -19,30 +20,40 @@ const HeroContainer = styled.div`
   width: 100%;
 `;
 
-const Home = () => (
-  <Layout>
-    <Head>
-      <title>{siteTitle}</title>
-    </Head>
+const Home = () => {
+  const auth = useAuth();
 
-    <HeroSection>
-      <HeroContainer>
-        <h1>
-          A file system
-          <br />
-          for the web
-        </h1>
-        <p>
-          {siteTitle} helps you bookmark and organize all your links in folders
-          <br />
-          so you can quickly grab them whenever you need them.
-        </p>
-        <h4>We’re in private beta. Drop in your email for updates.</h4>
-        <button onClick={(e) => auth.signinWithGitHub()}></button>
-        <Subscribe />
-      </HeroContainer>
-    </HeroSection>
-  </Layout>
-);
+  return (
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <HeroSection>
+        <HeroContainer>
+          <h1>
+            A file system
+            <br />
+            for the web
+          </h1>
+          <p>
+            {siteTitle} helps you bookmark and organize all your links in
+            folders
+            <br />
+            so you can quickly grab them whenever you need them.
+          </p>
+          <h4>
+            We’re in private beta. Drop in your email to join the waiting list.
+          </h4>
+          <button onClick={(e) => auth.signinWithGitHub()}>
+            Login with Github
+          </button>
+          <div>{auth?.user?.email}</div>
+          <Subscribe />
+        </HeroContainer>
+      </HeroSection>
+    </Layout>
+  );
+};
 
 export default Home;
