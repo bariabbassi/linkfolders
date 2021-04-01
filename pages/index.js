2822b2a4fe4ea0a8a91b762b09326214cff3fab8
Bari Abbassi <bari@abbas.si>
2021-04-01 16:42:33 +0100

    replace react-form-hook with onChange

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Head from 'next/head';
 import {
   Button,
@@ -14,7 +14,6 @@ import {
 } from '@chakra-ui/react';
 import { ArrowForwardIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
-import { useForm } from 'react-hook-form';
 
 import LandingShell from '@/components/LandingShell';
 import { useAuth } from '@/lib/auth';
@@ -23,20 +22,45 @@ import { checkUsername } from '@/lib/db';
 const Home = () => {
   const auth = useAuth();
   const [username, setUsername] = useState();
-  const { register, handleSubmit, errors } = useForm({
-    mode: 'onChange'
-  });
+  const [form, setForm] = useState(false);
+  const inputEl = useRef(null);
 
-  const onCheckUsername = (values) => {
-    onsole.log('onSubmit');
-    console.log('username', values.username);
-    console.log('errors', errors.username);
-  };
+  const subscribe = async (e) => {
+    setForm({ state: 'loading', message: '⏳ Loading...' });
+    const username = e.target.value;
+    const regEx = /^([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)$/;
 
-  const onChange = (username) => {
-    console.log('onChange');
-    console.log('username', username);
-    console.log('errors', errors.username);
+    if (username.length == 0) {
+      setForm({
+        state: 'empty',
+        message: ''
+      });
+    } else if (username?.length < 3) {
+      setForm({
+        state: 'error',
+        message: `😥 Sorry! @${username} is too short`
+      });
+    } else if (username?.length > 30) {
+      setForm({
+        state: 'error',
+        message: `😥 Sorry! @${username} is too long`
+      });
+    } else if (!regEx.test(username)) {
+      setForm({
+        state: 'error',
+        message: `😥 Sorry! @${username} is not valid`
+      });
+    } else if (!checkUsername(username)) {
+      setForm({
+        state: 'error',
+        message: `😥 Sorry! @${username} is already taken`
+      });
+    } else {
+      setForm({
+        state: 'success',
+        message: `🎉 Hooray! @${username} is available`
+      });
+    }
   };
 
   return (
@@ -67,57 +91,58 @@ const Home = () => {
 
       <Flex
         as="form"
-        align="center"
+        direction="column"
         justify="stretch"
         spacing={1}
         maxW="460px"
         w="100%"
-        onSubmit={handleSubmit(onCheckUsername)}
-        onChange={(e) => onChange(e.target.value)}
       >
-        <InputGroup size="lg">
-          <Input
-            py={6}
-            pl="9.6rem"
-            type="username"
-            name="username"
-            placeholder="yourusername"
-            value={username}
-            ref={register({
-              minLength: 3,
-              maxLength: 30,
-              pattern: /([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)/g
-              // validate: (input) => checkUsername(input)
-            })}
-            // isInvalid={errors.username}
-            focusBorderColor={errors.username !== undefined && 'crimson'}
-          />
+        <Stack direction="row">
+          <InputGroup size="lg">
+            <Input
+              py={6}
+              pl="9.6rem"
+              type="username"
+              name="username"
+              placeholder="yourusername"
+              value={username}
+              onChange={subscribe}
+            />
 
-          <InputLeftElement w="9.5rem">
-            <Flex
-              pl={6}
-              pt="0.13rem"
-              direction="column"
-              align="center"
-              justify="center"
+            <InputLeftElement w="9.5rem">
+              <Flex
+                pl={6}
+                pt="0.13rem"
+                direction="column"
+                align="center"
+                justify="center"
+              >
+                <Text>📂 linkfolde.rs/</Text>
+              </Flex>
+            </InputLeftElement>
+          </InputGroup>
+          <NextLink href={`/sigup?username=${username}`} passHref>
+            <Button
+              as="a"
+              ml={2}
+              size="lg"
+              variant="solid"
+              colorScheme="yellow"
+              rightIcon={<ArrowForwardIcon />}
             >
-              <Text>📂 linkfolde.rs/</Text>
-            </Flex>
-          </InputLeftElement>
-        </InputGroup>
-        <Text>errors: {errors.username?.type}</Text>
-        <NextLink href={`/sigup?username=${username}`} passHref>
-          <Button
-            as="a"
-            ml={2}
-            size="lg"
-            variant="solid"
-            colorScheme="yellow"
-            rightIcon={<ArrowForwardIcon />}
-          >
-            Sign up
-          </Button>
-        </NextLink>
+              Sign up
+            </Button>
+          </NextLink>
+        </Stack>
+        <Box m={2} fontWeight="600">
+          {form.state === 'loading' ? (
+            <Text>{form.message}</Text>
+          ) : form.state === 'error' ? (
+            <Text color="red.500">{form.message}</Text>
+          ) : form.state === 'success' ? (
+            <Text color="green.400">{form.message}</Text>
+          ) : null}
+        </Box>
       </Flex>
     </LandingShell>
   );
