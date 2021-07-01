e1796eef76b9f19b5f7f8b94c66f5e6d83b6f18f
Bari Abbassi <bari@abbas.si>
2021-07-01 22:26:30 +0100

    folder user profile working

--- components/Folder/FolderHeader.js
+++ components/Folder/FolderHeader.js
@@ -1,28 +1,30 @@
-import { Flex, Stack, Avatar, Heading } from '@chakra-ui/react';
+import {
+  Flex,
+  Stack,
+  Button,
+  Avatar,
+  Heading,
+  Tag,
+  TagLabel,
+  LinkBox,
+  LinkOverlay,
+  Spinner
+} from '@chakra-ui/react';
 import useSWR from 'swr';
+import NextLink from 'next/link';
 
 import fetcher from '@/utils/fetcher';
 import { useAuth } from '@/lib/auth';
 
 const FolderHeader = ({ name, userId }) => {
   const auth = useAuth();
-  const { data } = useSWR(`/api/profiles/${userId}`, fetcher);
-
-  if (auth) console.log(auth.user.profile);
-  // let profile = {};
-  // if (auth?.user && data?.folder?.userId !== auth?.user?.uid) {
-  //   profile = {
-  //     name: profileData?.profile?.name,
-  //     username: profileData?.profile?.username,
-  //     photoUrl: profileData?.profile?.photoUrl
-  //   };
-  // } else {
-  //   profile = {
-  //     name: auth?.user.name,
-  //     username: auth?.user.username,
-  //     photoUrl: auth?.user.photoUrl
-  //   };
-  // }
+  const { data } = useSWR(
+    !auth || userId !== auth?.user?.uid ? `/api/profiles/${userId}` : null,
+    fetcher
+  );
+  let profile = {};
+  if (auth && userId === auth?.user?.uid) profile = auth?.user?.profile;
+  else profile = data?.profile;
 
   return (
     <Flex w="100%" as="header">
@@ -31,17 +33,32 @@ const FolderHeader = ({ name, userId }) => {
           {name}
         </Heading>
         {/* <Heading size="md" fontWeight="400" mb={5} color="grey"></Heading> */}
-        <Flex align="center">
-          <Avatar
-            size="sm"
-            name={data?.profile?.name}
-            src={data?.profile?.photoUrl}
-            // src={profile?.photoUrl}
-          />
-          <Heading size="sm" fontWeight="400" color="grey" ml={2}>
-            {data?.profile?.name}
-          </Heading>
-        </Flex>
+        {profile?.username ? (
+          <NextLink href={`/${profile?.username}`} passHref>
+            <LinkBox as="article">
+              <Tag size="lg" borderRadius="full">
+                <Avatar
+                  size="xs"
+                  ml={-1}
+                  mr={2}
+                  name={profile?.name}
+                  src={profile?.photoUrl}
+                />
+                <TagLabel size="sm" fontWeight="400" mr={2}>
+                  <LinkOverlay href="#">{profile?.name}</LinkOverlay>
+                </TagLabel>
+              </Tag>
+            </LinkBox>
+          </NextLink>
+        ) : (
+          <Tag size="lg" borderRadius="full" w="140px">
+            {/* <Avatar size="xs" ml={-1} /> */}
+            <Spinner ml={-1} mr={3} />
+            <TagLabel size="sm" fontWeight="400">
+              Loading...
+            </TagLabel>
+          </Tag>
+        )}
       </Stack>
     </Flex>
   );
