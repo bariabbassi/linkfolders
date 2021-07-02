e9a78eb31f0b17cf99099d3859da87253410772a
Bari Abbassi <bari@abbas.si>
2021-07-02 18:17:58 +0100

    add edit profile button

--- components/Profile/ProfileHeader.js
+++ components/Profile/ProfileHeader.js
@@ -1,16 +1,28 @@
-import { Flex, Stack, Avatar, Heading } from '@chakra-ui/react';
+import { Flex, Stack, Avatar, Heading, Button } from '@chakra-ui/react';
 
 const ProfileHeader = ({ name, photoUrl, username }) => (
   <Flex align="space-between" w="100%" as="header">
     <Avatar bg="gray.200" m={3} size="xl" name={name} src={photoUrl} />
-    <Stack spacing={1} mx={3} my={4}>
-      <Heading as="h1" size="xl">
-        {name}
-      </Heading>
+    <Flex direction="column" w="100%" mx={3} my={4}>
+      <Flex align="center">
+        <Heading as="h1" size="xl">
+          {name}
+        </Heading>
+        <Button
+          variant="outline"
+          borderRadius="full"
+          size="md"
+          fontWeight="400"
+          ml={4}
+        >
+          Edit profile
+        </Button>
+      </Flex>
+
       <Heading size="md" fontWeight="400" mb={5} color="gray">
         @{username}
       </Heading>
-    </Stack>
+    </Flex>
   </Flex>
 );
 
