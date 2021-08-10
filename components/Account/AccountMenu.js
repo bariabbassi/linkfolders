29864ef3a10a05ecaa3da99576c8bc69bed38534
Bari Abbassi <bari@abbas.si>
2021-08-10 18:33:13 +0100

    account menu

--- components/Account/AccountMenu.js
+++ components/Account/AccountMenu.js
@@ -1,14 +1,15 @@
 import {
+  Box,
+  HStack,
+  IconButton,
+  Avatar,
+  Heading,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
-  MenuDivider,
-  IconButton,
-  Avatar,
-  Icon
+  MenuDivider
 } from '@chakra-ui/react';
-import { SettingsIcon } from '@chakra-ui/icons';
 import { User, Settings, LogOut } from 'react-feather';
 import NextLink from 'next/link';
 
@@ -23,21 +24,36 @@ const AccountMenu = () => {
       <MenuButton
         as={IconButton}
         variant="ghost"
-        borderRadius="full"
+        borderRadius="2xl"
         aria-label="Account menu"
+        _hover={{ bg: 'gray.200' }}
         size="xl"
+        w="100%"
         p={1}
-        mr={2}
-        icon={
+        mb={9}
+      >
+        <HStack>
           <Avatar
             color="gray.600"
             bg="gray.200"
-            size="md"
-            name={auth?.user?.profile?.name}
-            src={auth?.user?.profile?.photoUrl}
+            size="lg"
+            h="3.5rem"
+            w="3.5rem"
+            borderRadius="2xl"
+            mr={1}
+            name={auth.user?.profile?.name}
+            src={auth.user?.profile?.photoUrl}
           />
-        }
-      />
+          <Box>
+            <Heading as="h1" size="md">
+              {auth.user?.profile?.name}
+            </Heading>
+            <Heading size="xs" fontWeight="300" color="gray">
+              @{auth.user?.profile?.username}
+            </Heading>
+          </Box>
+        </HStack>
+      </MenuButton>
       <MenuList>
         <NextLink href={`/${auth?.user?.profile?.username}`} passHref>
           <MenuItem icon={<User size={19} />}>Profile</MenuItem>
--- components/Sidebar/SidebarContent.js
+++ components/Sidebar/SidebarContent.js
@@ -1,20 +1,11 @@
-import {
-  Box,
-  CloseButton,
-  Flex,
-  HStack,
-  Text,
-  Button,
-  IconButton,
-  Avatar,
-  Heading
-} from '@chakra-ui/react';
+import { Box, Flex, VStack, IconButton } from '@chakra-ui/react';
 import { CloseIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
 import useSWR from 'swr';
 
 import fetcher from '@/utils/fetcher';
 import { useAuth } from '@/lib/auth';
+import AccountMenu from '@/components/Account/AccountMenu';
 import ChildrenList from '@/components/Folder/ChildrenList';
 import AddRootFolderButton from '@/components/Add/AddRootFolderButton';
 
@@ -31,58 +22,35 @@ const SidebarContent = ({ onClose, ...rest }) => {
       transition="3s ease"
       w={{ base: 'full', md: 96 }}
       h="full"
-      px={3}
       pos="fixed"
       bg="gray.100"
       {...rest}
     >
-      <Flex align="center" justify="space-between" h="20" mb={7}>
-        <NextLink href={`/${auth.user?.profile?.username}`} passHref>
-          <Button
-            as="a"
-            variant="ghost"
-            size="xl"
-            borderRadius="2xl"
-            fontWeight="400"
-          >
-            <HStack as="header" w="100%">
-              <Avatar
-                color="gray.600"
-                bg="gray.200"
-                size="lg"
-                h="3.5rem"
-                w="3.5rem"
-                borderRadius="2xl"
-                mr={1}
-                name={auth.user?.profile?.name}
-                src={auth.user?.profile?.photoUrl}
-              />
-              <Box>
-                <Heading as="h1" size="md">
-                  {auth.user?.profile?.name}
-                </Heading>
-                <Heading size="xs" fontWeight="300" color="gray">
-                  @{auth.user?.profile?.username}
-                </Heading>
-              </Box>
-            </HStack>
-          </Button>
-        </NextLink>
+      <Flex
+        display={{ base: 'flex', md: 'none' }}
+        ml={{ base: 0, md: 96 }}
+        px={{ base: 1, md: 2 }}
+        h={{ base: 12, md: 14 }}
+        align="center"
+        justify="flex-end"
+      >
         <IconButton
-          display={{ base: 'flex', md: 'none' }}
           variant="ghost"
           size="md"
-          aria-label="Close SidebarContent"
+          aria-label="Close sidebar"
           icon={<CloseIcon boxSize={4} color="gray" />}
           onClick={onClose}
         />
       </Flex>
-      <ChildrenList
-        folderId={folderId}
-        childrenOrder={data?.folder?.children}
-        editable={true}
-      />
-      <AddRootFolderButton />
+      <Flex direction="column" align="flex-start" px={3} py={2}>
+        <AccountMenu />
+        <ChildrenList
+          folderId={folderId}
+          childrenOrder={data?.folder?.children}
+          editable={true}
+        />
+        <AddRootFolderButton />
+      </Flex>
     </Box>
   );
 };
